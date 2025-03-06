bb8a9278db92b3b6d3640f53159438320a6d1f36
Yosef Rezaie <yosefrezaie5001@gmail.com>
2025-03-06 20:17:13 +0330

    complete home page

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,9 @@
-import Image from "next/image";
 import { Geist, Geist_Mono } from "next/font/google";
+import Categories from "@/components/modules/Categories";
+import SearchBar from "@/components/modules/SearchBar";
+import CarsPage from "@/components/templates/CarsPage";
+import carsData from "@/data/carsData";
+import AllButton from "@/components/modules/AllButton";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -12,9 +16,13 @@ const geistMono = Geist_Mono({
 });
 
 export default function Home() {
+  const cars = carsData.slice(0, 3);
   return (
-    <div className="">
-      hello
+    <div>
+      <SearchBar />
+      <Categories />
+      <AllButton />
+      <CarsPage data={cars} />
     </div>
   );
 }
