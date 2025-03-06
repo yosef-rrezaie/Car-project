c72279ffbca50fe7f99e6a8fb4e4a672f63993d2
yosef-rrezaie <yosefrrezaie@gmail.com>
2025-03-06 12:22:45 +0330

    crete dynamic route for details car

--- components/modules/Card.js
+++ components/modules/Card.js
@@ -1,20 +1,23 @@
+import Link from "next/link";
 import Location from "../icons/Location";
 import styles from "./Card.module.css";
 export default function Card(props) {
   const { id, name, model, year, distance, location, image, price } =
     props.data;
   return (
-    <div className={styles.container}>
-      <img src={image} className={styles.image} />
-      <h4 className={styles.title}>{`${name} ${model}`}</h4>
-      <p className={styles.detail}>{`${year} - ${distance} km`}</p>
-      <div className={styles.footer}>
-        <p>$ {price}</p>
-        <div className={styles.location}>
+    <Link href={`/cars/${id}`}>
+      <div className={styles.container}>
+        <img src={image} className={styles.image} />
+        <h4 className={styles.title}>{`${name} ${model}`}</h4>
+        <p className={styles.detail}>{`${year} - ${distance} km`}</p>
+        <div className={styles.footer}>
+          <p>$ {price}</p>
+          <div className={styles.location}>
             <p>{location}</p>
-            <Location/>
+            <Location />
+          </div>
         </div>
       </div>
-    </div>
+    </Link>
   );
 }
