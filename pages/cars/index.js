d3ed7de5efb387ef0aca05400d81a7987d0943cd
yosef-rrezaie <yosefrrezaie@gmail.com>
2025-03-06 13:09:10 +0330

    add category section

--- pages/cars/index.js
+++ pages/cars/index.js
@@ -1,9 +1,11 @@
+import Categories from "@/components/modules/Categories";
 import CarsPage from "@/components/templates/CarsPage";
 import carsData from "@/data/carsData";
 
 function Details() {
   return (
     <div>
+      <Categories/>
       <CarsPage data={carsData} />
     </div>
   );
